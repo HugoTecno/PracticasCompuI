ecd2fef9c1b05cd1e027dcb99ea4b2a589207746
HugoTecno <113045767+HugoTecno@users.noreply.github.com>
2022-09-27 21:52:48 -0500

    Codigo Final

--- Calculadora/Calculadora.cpp
+++ Calculadora/Calculadora.cpp
@@ -1,14 +1,27 @@
 // Autor: Victor Hugo Mejia Trejo
 // Fecha: 27 Septiembre 2022
-
-
 #include <iostream>
-using namespace std;
-int main {
-  cout << "Calculadora de operaciones basicas. Bienvenido:" << endl;
-  
-  
-  
-  
-  {
+using namespace std ;
+int main() {
+    //Este programa hace operaciones entre dos Numeros.
+    int n1,n3;
+    char n2;
+    cout <<"Calculadora de dos digitos"<<endl ;
+    cout <<"Ingrese el primer numero:"<<endl ;
+    cin >> n1;
+    cout << "Que operacion desea realizar:"<< endl;
+    cout << "Suma(+) Resta(-) Multiplicacion(*) Division(/) Modulo(%)"<< endl;
+    cout << "Ingrese solo el simbolo de la operacion:"<< endl;
+    cin >> n2;
+    cout << "Ingrese el segundo numero:"<< endl;
+    cin >> n3;
+
+    if (n2 == '+') {cout  << "Resultado: " << n1 << n2 << n3 << " = " << n1 + n3 << endl;}
+    if (n2 == '-') {cout  << "Resultado: " << n1 << n2 << n3 << " = " << n1 - n3 << endl;}
+    if (n2 == '/') {cout  << "Resultado: " << n1 << n2 << n3 << " = " << n1 / n3 << endl;}
+    if (n2 == '*') {cout  << "Resultado: " << n1 << n2 << n3 << " = " << n1 * n3 << endl;}
+    if (n2 == '%') {cout  << "Resultado: " << n1 << n2 << n3 << " = " << n1 % n3 << endl;}
+
+    return 0;
+}
 
