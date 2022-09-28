0483f599a1aa7f3eb1768e9488f2565d7b7de3cb
HugoTecno <113045767+HugoTecno@users.noreply.github.com>
2022-09-27 21:05:40 -0500

    Update Calculadora.cpp

--- Calculadora/Calculadora.cpp
+++ Calculadora/Calculadora.cpp
@@ -1,1 +1,14 @@
+// Autor: Victor Hugo Mejia Trejo
+// Fecha: 27 Septiembre 2022
+
+
+#include <iostream>
+using namespace std;
+int main {
+  cout << "Calculadora de operaciones basicas. Bienvenido:" << endl;
+  
+  
+  
+  
+  {
 
