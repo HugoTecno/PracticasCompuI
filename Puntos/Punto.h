6310c1639e41e7fcf8d6e3ced3e2d5575e3408d2
HugoTecno <113045767+HugoTecno@users.noreply.github.com>
2022-10-30 14:35:34 -0600

    Update Punto.h

--- Puntos/Punto.h
+++ Puntos/Punto.h
@@ -1,1 +1,27 @@
+#ifndef PUNTOS_PUNTO_H
+#define PUNTOS_PUNTO_H
+
+class Punto2D {
+//métpodos de set
+public:
+    void SetPosicion(float temp1, float temp2);
+    // métodos get
+    float GetX();
+    float GetY();
+
+    void Trasladar(int temp1, int temp2);
+    void RotarRespectoAlOrigen(int temp1);
+    void Escalar(float temp1, float temp2);
+
+
+private:
+    float x,y;
+
+
+};
+
+
+
+
+#endif //PUNTOS_PUNTO_H
 
