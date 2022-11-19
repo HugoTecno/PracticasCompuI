6fe546146f52d3df0eeb9080440ccedb708c695b
HugoTecno <113045767+HugoTecno@users.noreply.github.com>
2022-11-18 18:36:04 -0600

    Update Punto.cpp

--- Puntos/Punto.cpp
+++ Puntos/Punto.cpp
@@ -1,30 +1,32 @@
 #include "Punto.h"
-#include <math.h>
+#include "cmath"
 
 void Punto2D::SetPosicion(float temp1, float temp2) {
-    x = temp1;
-    y = temp2;
+    x=temp1;
+    y=temp2;
 }
 
 float Punto2D::GetX() {
     return x;
-
 }
+
 float Punto2D::GetY() {
     return y;
 }
+
+void Punto2D::Trasladar(float temp1, float temp2) {
+    x=x+temp1;
+    y=y+temp2;
+}
+
 void Punto2D::Escalar(float temp1, float temp2) {
     x=x*temp1;
     y=y*temp2;
+}
 
-}
-void Punto2D::RotarRespectoAlOrigen(int temp1) {
+void Punto2D::RotarRespectoAlOrigen(float temp1) {
     float aux;
-    aux=x;
-    x=((x)*cos(temp1*3.1416/180))-((y)*(sin(temp1*3.1416/180)));
-    y=((aux)*sin(temp1*3.1416/180))+((y)*(cos(temp1*3.1416/180)));
-}
-void Punto2D::Trasladar(int temp1, int temp2) {
-x=x+temp1;
-y=y+temp2;
+    aux = x;
+    x = (x * cos(temp1*3.141617/180)) - (y * sin(temp1*3.141617/180));
+    y = (aux * sin(temp1*3.141617/180)) + (y * cos(temp1*3.141617/180));
 }
