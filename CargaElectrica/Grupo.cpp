061ea185d636d8892bc98944c45f9633763d4db2
HugoTecno <113045767+HugoTecno@users.noreply.github.com>
2022-11-20 09:39:24 -0600

    Update Grupo.cpp

--- CargaElectrica/Grupo.cpp
+++ CargaElectrica/Grupo.cpp
@@ -1,1 +1,21 @@
-
+# include "Grupo.h"
+using namespace std;
+Grupo::Grupo(string nom, float p , int c) {
+nombre=nom;
+cargaGrupo=c;
+PkGrupo=p;
+}
+Grupo::Grupo() {
+    nombre="Hugo";
+    cargaGrupo=0;
+    PkGrupo=0;
+}
+string Grupo::GetN() {
+    return nombre;
+}
+float Grupo::Getpk() {
+    return PkGrupo;
+}
+int Grupo::GetCar() {
+    return cargaGrupo;
+}
